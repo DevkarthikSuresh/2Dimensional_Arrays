0d70ec27f1cb1e2af713747321e32eaa2ae22c71
Devkarthik <devkarthik300@gmail.com>
2024-08-27 12:29:07 +0530

    Update matrix_multplication.cpp

--- matrix_multplication.cpp
+++ matrix_multplication.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     int r1, c1, r2, c2;
 
-    // Input sizes of matrices
+    
     cout << "Enter the number of rows of the first matrix: ";
     cin >> r1;
     cout << "Enter the number of columns of the first matrix: ";
@@ -18,16 +18,16 @@ int main() {
     cout << "Enter the number of columns of the second matrix: ";
     cin >> c2;
 
-    // Check if matrix multiplication is possible
+    
     if (c1 != r2) {
         cout << "Matrix multiplication is not possible. Number of columns in the first matrix must equal the number of rows in the second matrix." << endl;
         return 1;
     }
 
-    // Declare matrices
+    
     int A[r1][c1], B[r2][c2], C[r1][c1] = {0};
 
-    // Input elements for the first matrix
+    
     cout << "Enter elements of the first matrix:" << endl;
     for (int i = 0; i < r1; ++i) {
         for (int j = 0; j < c1; ++j) {
@@ -36,7 +36,7 @@ int main() {
         }
     }
 
-    // Input elements for the second matrix
+    
     cout << "Enter elements of the second matrix:" << endl;
     for (int i = 0; i < c2; ++i) {
         for (int j = 0; j < c2; ++j) {
@@ -45,7 +45,7 @@ int main() {
         }
     }
 
-    // Multiply matrices
+   
     for (int i = 0; i < r1; ++i) {
         for (int j = 0; j < c1; ++j) {
             for (int k = 0; k < c1; ++k) {
@@ -54,7 +54,7 @@ int main() {
         }
     }
 
-    // Display the result
+    
     cout << "The resultant matrix is:" << endl;
     for (int i = 0; i < r1; ++i) {
         for (int j = 0; j < c1; ++j) {
