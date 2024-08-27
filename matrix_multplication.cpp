3de59de60817d4f724649d3d6f87f20a3b673926
Devkarthik <devkarthik300@gmail.com>
2024-08-27 12:18:01 +0530

    Update matrix_multplication.cpp

--- matrix_multplication.cpp
+++ matrix_multplication.cpp
@@ -2,64 +2,66 @@
 //PRN:23070123045
 //Aim: Using 2D arrays for matrix multiplicatiion
 #include <iostream>
-
 using namespace std;
 
 int main() {
-  
-    const int r1 = 2, c1 = 3; 
-    const int r2 = 3, c2 = 2; 
+    int r1, c1, r2, c2;
 
-   
-    int A[r1][c1] = { {1, 2, 3}, {4, 5, 6} };
-    int B[r2][c2] = { {7, 8}, {9, 10}, {11, 12} };
-    int C[r1][c2] = {0}; 
+    // Input sizes of matrices
+    cout << "Enter the number of rows of the first matrix: ";
+    cin >> r1;
+    cout << "Enter the number of columns of the first matrix: ";
+    cin >> c1;
 
-    
-    if (c1 == r2) {
+    cout << "Enter the number of rows of the second matrix: ";
+    cin >> r2;
+    cout << "Enter the number of columns of the second matrix: ";
+    cin >> c2;
 
-       cout << "A matrix: "<< endl;
-       for (int i = 0; i < r1; ++i) {
-        for (int j = 0; j < c2; ++j) {
-            C[i][j] = 0; 
-           
-                cout << A[i][j] << " ";
-            }
-         cout << endl;
+    // Check if matrix multiplication is possible
+    if (c1 != r2) {
+        cout << "Matrix multiplication is not possible. Number of columns in the first matrix must equal the number of rows in the second matrix." << endl;
+        return 1;
+    }
+
+    // Declare matrices
+    int A[r1][c1], B[r2][c2], C[r1][c1] = {0};
+
+    // Input elements for the first matrix
+    cout << "Enter elements of the first matrix:" << endl;
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c1; ++j) {
+            cout << "A[" << i << "][" << j << "]: ";
+            cin >> A[i][j];
         }
-      
-      cout << "B matrix: "<< endl;
-      for (int i = 0; i < r1; ++i) {
-        for (int j = 0; j < c2; ++j) {
-            C[i][j] = 0; 
-           
-                cout << B[i][j] << " ";
-            }
-         cout << endl;
-        }
+    }
 
-       for (int i = 0; i < r1; ++i) {
+    // Input elements for the second matrix
+    cout << "Enter elements of the second matrix:" << endl;
+    for (int i = 0; i < c2; ++i) {
         for (int j = 0; j < c2; ++j) {
-            C[i][j] = 0; 
+            cout << "B[" << i << "][" << j << "]: ";
+            cin >> B[i][j];
+        }
+    }
+
+    // Multiply matrices
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c1; ++j) {
             for (int k = 0; k < c1; ++k) {
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 
-    cout << "Result matrix C:" << endl;
+    // Display the result
+    cout << "The resultant matrix is:" << endl;
     for (int i = 0; i < r1; ++i) {
-        for (int j = 0; j < c2; ++j) {
+        for (int j = 0; j < c1; ++j) {
             cout << C[i][j] << " ";
         }
         cout << endl;
     }
-       
-        
-    }
-else { cout << "Multiplication is not possible. The number of columns of A must be equal to the number of rows of B." ;}
-    
-   
 
     return 0;
 }
