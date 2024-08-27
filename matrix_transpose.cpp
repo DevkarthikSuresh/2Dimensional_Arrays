7dd7c370708775907b4e94a50265aff0f9f4fb20
Devkarthik <devkarthik300@gmail.com>
2024-08-27 12:59:27 +0530

    Update matrix_transpose.cpp

--- matrix_transpose.cpp
+++ matrix_transpose.cpp
@@ -6,12 +6,9 @@ using namespace std;
 
 int main() {
     int a[3][3] = {{2,3,4},{4,5,6},{5,6,7}};
+    int b[3][3];
 
-for(int i = 0;i<3;i++){
-    for(int j = 0;j<3;j++){
-        a[i][j] = a[j][i];
-    }
-}
+
   cout << "The original matrix is:"<<endl;
     for(int i = 0;i<3;i++){
     for(int j = 0;j<3;j++){
@@ -22,25 +19,24 @@ for(int i = 0;i<3;i++){
 cout << endl;
  cout << "The tranpose of matrix is: "<<endl;
 for(int i = 0;i<3;i++){
-    cout << endl;
     for(int j = 0;j<3;j++){
-        cout << a[j][i] << " ";
+        b[i][j] = a[j][i];
+        cout << b[i][j] << " ";
     }
-} 
+    cout << endl;
+}
 
     return 0;
 }
-
 /*
 Output
 The original matrix is:
-2 4 5 
+2 3 4 
 4 5 6 
 5 6 7 
 
 The tranpose of matrix is: 
-
 2 4 5 
-4 5 6 
-5 6 7 
+3 5 6 
+4 6 7  
 */
